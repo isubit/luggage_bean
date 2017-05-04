8bb9e0c68d7499d07056b84aebf63ffd44b3eedb
Angela McMahon <angelam@iastate.edu>
2017-05-04 14:54:45 -0500

    LUGG-989 Added validation to see if image exists in template

--- templates/bean--front-block.tpl.php
+++ templates/bean--front-block.tpl.php
@@ -1,6 +1,6 @@
 <div class="image-link link-<?php print $bean->field_front_block_label_color['und'][0]['value']; ?><?php print $attributes; ?>>
   <a href="<?php print $bean->field_front_block_url['und'][0]['url']; ?>">
-    <img src="<?php print file_create_url($bean->field_front_block_image['und'][0]['uri']); ?>" alt="<?php print $bean->field_front_block_image['und'][0]['alt']; ?>" />
+    <?php if (isset($bean->field_front_block_image['und'][0]['uri'])): ?><img src="<?php print file_create_url($bean->field_front_block_image['und'][0]['uri']); ?>" alt="<?php print $bean->field_front_block_image['und'][0]['alt']; ?>" /><?php endif; ?>
       <h2><?php if (isset($bean->field_front_block_label_icon['und'][0]['value'])): ?><i class="fa <?php print $bean->field_front_block_label_icon['und'][0]['value']; ?>"></i><?php endif; print $title; ?></h2>
   </a>
 </div>
