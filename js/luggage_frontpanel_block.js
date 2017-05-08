1e7c79f84e7a843a5eedee2ed9f810d3e823db41
Angela McMahon <angelam@iastate.edu>
2017-05-08 12:59:53 -0500

    LUGG-989 Added margin to fontawesome icon preview

--- js/luggage_frontpanel_block.js
+++ js/luggage_frontpanel_block.js
@@ -3,7 +3,7 @@
     $('select#edit-field-front-block-label-icon-und').change(function(){
         if ($('select#edit-field-front-block-label-icon-und') != '_none') {
           if ($('i.select').length == 0) {
-            $('<i class=\"fa '+ $('select#edit-field-front-block-label-icon-und').val() +' select\"></i>').insertAfter("#edit-field-front-block-label-icon-und");
+            $('<i class=\"fa '+ $('select#edit-field-front-block-label-icon-und').val() +' select\" style=\"margin: 1rem 0 0 1rem\"></i>').insertAfter("#edit-field-front-block-label-icon-und");
           } else {
             $('i.select').attr('class', 'fa '+ $('select#edit-field-front-block-label-icon-und').val() +' select');
           }
