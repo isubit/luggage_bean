c6b563c7b13e960c7255837db3fdbe4d7e93b1f5
Carney, Sarah A <sacarney@iastate.edu>
2017-10-18 15:59:07 -0500

    LUGG-989 Updated template to use new field as a class

--- templates/bean--link-block.tpl.php
+++ templates/bean--link-block.tpl.php
@@ -1,4 +1,4 @@
-<div class="image-link link-<?php print $bean->field_link_block_label_color['und'][0]['value']; ?><?php print $attributes; ?>>
+<div class="<?php print $bean->field_link_block_style['und'][0]['value'] ?> <?php print $attributes; ?>>
   <a href="<?php print $bean->field_link_block_url['und'][0]['url']; ?>">
     <?php if (isset($bean->field_link_block_image['und'][0]['uri']) && isset($bean->field_link_block_image_style['und'][0]['value'])): ?><img src="<?php print image_style_url($bean->field_link_block_image_style['und'][0]['value'], $bean->field_link_block_image['und'][0]['uri']); ?>" alt="<?php print $bean->field_link_block_image['und'][0]['alt']; ?>" />
     <?php elseif (isset($bean->field_link_block_image['und'][0]['uri'])): ?><img src="<?php print file_create_url($bean->field_link_block_image['und'][0]['uri']); ?>" alt="<?php print $bean->field_link_block_image['und'][0]['alt']; ?>" /><?php endif; ?>
