f5de5ee217a6b158fc88e46b23ef60a26a7e6bda
Carney, Sarah A <sacarney@iastate.edu>
2018-12-05 10:27:05 -0600

    Adding back in a few things that went missing between master to development merge

--- templates/bean--card.tpl.php
+++ templates/bean--card.tpl.php
@@ -1,10 +1,5 @@
-<?php 
-  $class = NULL; 
-  if (isset($bean->field_card_width['und'][0]['value']) && $bean->field_card_width['und'][0]['value'] == 1 ) : 
-    $class = 'bean-card_width_full'; 
-  endif; ?>
+<div class="bean-card <?php if (!empty($bean->field_card_style['und'][0]['value'])): print $bean->field_card_style['und'][0]['value']; endif; ?> <?php if (!empty($bean->field_card_width['und'][0]['value'])): print 'bean-card_width_full'; endif; ?>" <?php print $attributes; ?>>
 
-<div class="bean-card <?php if (!empty($bean->field_card_style['und'][0]['value'])): print $bean->field_card_style['und'][0]['value']; endif; ?>" <?php print $attributes; ?>>
   <?php if (isset($bean->field_card_url['und'][0]['url'])): ?><a class="bean-card_link" href="<?php print $bean->field_card_url['und'][0]['url']; ?>"><?php else: ?><div class="bean-card_wrap"><?php endif ?>
 
     <?php if (isset($bean->field_card_image['und'][0]['uri'])): ?>
